a441e4f38f28e3e7512f478b3d4bbbb801af847b
Gorbatenko Vitaliy <gorvit.dp@gmail.com>
2021-04-11 12:03:05 +0300

    add allureTestOps build.gradle settings

--- src/test/java/tests/QaGuruTest.java
+++ src/test/java/tests/QaGuruTest.java
@@ -7,7 +7,6 @@ import com.github.javafaker.Faker;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
-import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
